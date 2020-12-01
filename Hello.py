5598da1c081ee95ca62ca620d9dc30d200aefa75
nobumacsuzuki <nobumac.suzuki@gmail.com>
2020-11-30 21:54:10 -0800

    initial push

---
+++ Hello.py
@@ -0,0 +1,17 @@
+message = 'Hello Aho'
+print(message)
+
+listSample = [0, 2, 3, 5]
+
+for temporal in listSample:
+    print(temporal)
+for temporalNumber in range(0, 10, 2):
+    print(temporalNumber)
+
+dictionarySample = {'one': 1, 'two':2, 'three':3}
+
+for temporalKey in dictionarySample:
+    print(temporalKey)
+
+for temporalValue in dictionarySample.values():
+    print(temporalValue)
